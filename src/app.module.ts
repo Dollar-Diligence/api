0bc1feb6bf8bb090bf066d0f49d77178d4967f14
AlmightyDuckman <115139566+MariusTusvik@users.noreply.github.com>
2023-06-06 12:22:45 +0200

    auth service: sign up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongodbModule } from './mongodb/mongodb.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongodbModule],
+  imports: [MongodbModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class RegisterDto {
+	@ApiProperty()
+	userName: string;
+
+	@ApiProperty()
+	email: string;
+
+	@ApiProperty()
+	password: string;
+}
